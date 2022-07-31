bb6c2f0b6f14391af8d1603e34e8c5accc512fd9
bhimk20 <94125747+bhimk20@users.noreply.github.com>
2022-07-31 19:31:16 +0530

    Implemented Tries Data Structure

---
+++ Tries/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tries;
+
+Node root = new();
+
+void Insert(string input)
+{
+    Node node = root;
+    for (int i = 0; i < input.Length; i++)
+    {
+        char ch = input[i];
+        if (!node.ContainsChar(ch))
+        {
+            node.Insert(ch, new());
+        }
+        node = node.Get(ch);
+    }
+
+    node.SetEnd(); 
+}
+
+bool Search(string input)
+{
+    Node node = root;
+    for (int i = 0; i < input.Length; i++)
+    {
+        char ch = input[i];
+        if (!node.ContainsChar(ch))
+        {
+            return false;
+        }
+        node = node.Get(ch);
+    }
+
+    return node.IsEnd();
+}
+
+bool StartsWith(string input)
+{
+    Node node = root;
+    for (int i = 0; i < input.Length; i++)
+    {
+        char ch = input[i];
+        if (!node.ContainsChar(ch))
+        {
+            return false;
+        }
+        node = node.Get(ch);
+    }
+
+    return true;
+}
+
+
+Insert("bhim");
+Console.WriteLine(Search("bhim"));
+Console.WriteLine(StartsWith("i"));
