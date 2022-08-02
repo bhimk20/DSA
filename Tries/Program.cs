a2f30565d6ba83403c6d403d0a7a93c903fea57c
bhimk20 <94125747+bhimk20@users.noreply.github.com>
2022-08-02 09:15:12 +0530

    Refactoring and Recursive Search in Tries

--- Tries/Program.cs
+++ Tries/Program.cs
@@ -1,56 +1,8 @@
 ﻿using Tries;
 
-Node root = new();
+Trie root = new();
 
-void Insert(string input)
-{
-    Node node = root;
-    for (int i = 0; i < input.Length; i++)
-    {
-        char ch = input[i];
-        if (!node.ContainsChar(ch))
-        {
-            node.Insert(ch, new());
-        }
-        node = node.Get(ch);
-    }
-
-    node.SetEnd(); 
-}
-
-bool Search(string input)
-{
-    Node node = root;
-    for (int i = 0; i < input.Length; i++)
-    {
-        char ch = input[i];
-        if (!node.ContainsChar(ch))
-        {
-            return false;
-        }
-        node = node.Get(ch);
-    }
-
-    return node.IsEnd();
-}
-
-bool StartsWith(string input)
-{
-    Node node = root;
-    for (int i = 0; i < input.Length; i++)
-    {
-        char ch = input[i];
-        if (!node.ContainsChar(ch))
-        {
-            return false;
-        }
-        node = node.Get(ch);
-    }
-
-    return true;
-}
-
-
-Insert("bhim");
-Console.WriteLine(Search("bhim"));
-Console.WriteLine(StartsWith("i"));
+root.Insert("bhim");
+Console.WriteLine(root.Search("bhim"));
+Console.WriteLine(root.RecursiveSearch("..m"));
+Console.WriteLine(root.StartsWith("bhi"));
---
+++ Tries/Trie.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tries
+{
+    public class Trie
+    {
+        Trie[] nodes = new Trie[26];
+        bool isEnd = false;
+
+        public void Insert(string word)
+        {
+            Trie node = this;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+                if (node.nodes[ch - 'a'] is null)
+                { 
+                    node.nodes[ch - 'a'] = new();
+                }
+                node = node.nodes[ch - 'a'];
+            }
+
+            node.isEnd = true;
+        }
+
+        public bool Search(string word)
+        {
+            Trie node = this;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+                if (node.nodes[ch - 'a'] is null)
+                {
+                    return false;
+                }
+                node = node.nodes[ch - 'a'];
+            }
+
+            return node.isEnd;
+        }
+
+        public bool StartsWith(string word)
+        {
+            Trie node = this;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+                if (node.nodes[ch - 'a'] is null)
+                {
+                    return false;
+                }
+                node = node.nodes[ch - 'a'];
+            }
+
+            return true;
+        }
+
+        public bool RecursiveSearch(string word)
+        {
+            return DFS(this, 0, word);
+        }
+
+        private bool DFS(Trie node, int index, string word)
+        {
+            if(node == null)
+            {
+                return false;
+            }
+            if(index == word.Length)
+            {
+                return node.isEnd;
+            }
+            char letter = word[index];
+            if (letter != '.')
+            {
+                node = node.nodes[letter - 'a'];
+                return DFS(node, (index + 1), word);
+            }
+            else
+            {
+                for(int j = 0; j < node.nodes.Length; j++)
+                {
+                    if(DFS(node.nodes[j], (index + 1), word))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
